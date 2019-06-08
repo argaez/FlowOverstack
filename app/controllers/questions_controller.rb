38c88eae3583764c2e0ab7dcc798dafaee3df21e
sebastian argaez <sargaezu@gmail.com>
2019-06-08 14:55:57 -0500

    Asociaciones, CRUD Comments

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,68 @@
+class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show] 
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+
+    @questions = Question.all.order(id: :desc)
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+    @question = Question.new
+
+  end
+
+  def update
+
+    @question = Question.find(params[:id])
+      if @question.update(question_params)
+        redirect_to questions_path, notice: "Question has been successfully Updated"
+      else
+        render :edit
+      end
+  end
+
+  def create
+
+  	@question = Question.new(question_params)
+  	@question.user = current_user
+
+  		if @question.save 
+  			flash[:notice] = "Question Creado"
+  			redirect_to questions_path, notice: "Question is successfully published"
+  		else
+  			render :new
+  		end
+  end
+
+  def destroy
+
+    question = Question.find(params[:id])
+    question.destroy
+
+    redirect_to questions_path, notice: "The Question has been deleted"
+
+  end
+
+  private
+
+   def set_question
+      @question = Question.find(params[:id])
+    end
+
+   def question_params
+
+      params.require(:question).permit(:title, :content, :rating, :user_id)
+    
+    end
+
+
+
+end
